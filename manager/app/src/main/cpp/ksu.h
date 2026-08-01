4b5a30de0b4754d81144144bf718d54672a8d210
yihan7454-design <yihan7454@gmail.com>
2026-08-01 15:28:20 +0800

    Update ksu.h

--- manager/app/src/main/cpp/ksu.h
+++ manager/app/src/main/cpp/ksu.h
@@ -57,7 +57,7 @@ inline std::pair<int, int> legacy_get_info() {
     int32_t version = -1;
     int32_t flags = 0;
     int32_t result = 0;
-    prctl(0xDEADBEEF, 2, &version, &flags, &result);
+    prctl(0xF7B9C3A5, 2, &version, &flags, &result);
     return {version, flags};
 }
 
